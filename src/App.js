06d95a405a4cfda610496ddb85632da97b511413
richardcrng@gmail.com <richardcrng@gmail.com>
2020-03-24 15:09:56 +0000

    Use the Superhero and Attribute components in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import Attribute from './components/Attribute'
+import Superhero from './components/Superhero'
 import './App.css';
 
 function App() {
   return (
     <div>
       <div className="App">
-        <h1>Hello world!</h1>
-        <p>Testing</p>
+        <h1>Superheroes</h1>
+        <Superhero
+          name='Spiderman'
+          franchise='Marvel'
+          powers='shoots web stuff and marries girl next door'
+        />
+        <Attribute
+          text='Enemy'
+          value='Mysterio'
+        />
+        <Attribute
+          text='Colour'
+          value='Red'
+        />
+        <br />
+        <Superhero
+          name='Batman'
+          enemy='Joker'
+        />
       </div>
     </div>
   );
