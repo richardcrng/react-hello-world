d23e23e1dc570e39d731350f60f1ce9e2fe00dab
richardcrng@gmail.com <richardcrng@gmail.com>
2020-03-25 11:43:10 +0000

    Implement some Semantic UI components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, Card, Icon, Image } from 'semantic-ui-react'
 import Attribute from './components/Attribute'
 import Superhero from './components/Superhero'
 import './App.css';
@@ -27,7 +28,26 @@ function App() {
           enemy='Joker'
         />
         <button>Here is my button</button>
+        <Button>Click here</Button>
       </div>
+      <Card>
+        <Image src='/assets/logo512.png' wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>Matthew</Card.Header>
+          <Card.Meta>
+            <span className='date'>Joined in 2015</span>
+          </Card.Meta>
+          <Card.Description>
+            Matthew is a musician living in Nashville.
+      </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a>
+            <Icon name='user' />
+        22 Friends
+      </a>
+        </Card.Content>
+      </Card>
     </div>
   );
 }
